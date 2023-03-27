4ab98b23cdf44e7c58b2ea3547676eb198253b83
AlexJYO <jesusgarcia0791@gmail.com>
2023-03-27 10:56:54 -0600

    Update getPointAdmin.php

--- getPointAdmin.php
+++ getPointAdmin.php
@@ -2,7 +2,7 @@
 $_post = json_decode(file_get_contents('php://input'), true);
 
 $codeAdmin = $_post['code'];
-
+date_default_timezone_set("America/Mexico_City");
 include('database.php');
 
 $db = new Database();
@@ -12,7 +12,7 @@ $conn = mysqli_connect($db->getHost(), $db->getUser(), $db->getPassword(), $db->
 if (mysqli_connect_errno()) {
     die("Failed to connect to MySQL:  " . mysqli_connect_error());
 }
-date_default_timezone_set("America/Mexico_City");
+
 
 $query = $db->connect()->prepare("SELECT n_point, r_point, sr_point, ur_point, point_1, point_2, point_3, point_4, point_5, point_6 FROM username WHERE code ='$codeAdmin'");
 $query->execute();
